d17f49a211e9f41ab48a16bb6734047124830ca8
Damian Karczmarz <damian.karczmarz92@gmail.com>
2021-04-17 15:33:48 +0200

    walidacja danych UserAdd (czy istnieją)

--- src/main/java/pl/coderslab/users/UserAdd.java
+++ src/main/java/pl/coderslab/users/UserAdd.java
@@ -17,15 +17,38 @@ public class UserAdd extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        UserDao userDao=new UserDao();
-        String userName=request.getParameter("userName");
-        String userEmail=request.getParameter("userEmail");
-        String userPassword=request.getParameter("userPassword");
-        User newUser=new User();
-        newUser.setUserName(userName);
-        newUser.setEmail(userEmail);
-        newUser.setPassword(userPassword);
-        newUser=userDao.create(newUser);
-        response.sendRedirect(request.getContextPath()+"/user/list");
+        UserDao userDao = new UserDao();
+        String userName = request.getParameter("userName");
+        String userEmail = request.getParameter("userEmail");
+        String userPassword = request.getParameter("userPassword");
+
+        if (userName.isEmpty() || userEmail.isEmpty() || userPassword.isEmpty()) {
+            setNotEmptyParameters(request, userName, userEmail, userPassword);
+            setWarningMessage(request,"Wszystkie pola muszą być wypełnione.");
+            getServletContext().getRequestDispatcher("/users/add.jsp").forward(request, response);
+        } else {
+            User newUser = new User();
+            newUser.setUserName(userName);
+            newUser.setEmail(userEmail);
+            newUser.setPassword(userPassword);
+            newUser = userDao.create(newUser);
+            response.sendRedirect(request.getContextPath() + "/user/list");
+        }
+    }
+
+    private void setWarningMessage(HttpServletRequest request, String message) {
+        request.setAttribute("message", message);
+    }
+
+    private void setNotEmptyParameters(HttpServletRequest request, String userName, String userEmail, String userPassword) {
+        if (!userName.isEmpty()) {
+            request.setAttribute("userName", userName);
+        }
+        if (!userEmail.isEmpty()) {
+            request.setAttribute("userEmail", userEmail);
+        }
+        if (!userPassword.isEmpty()) {
+            request.setAttribute("userPassword", userPassword);
+        }
     }
 }
--- src/main/webapp/users/add.jsp
+++ src/main/webapp/users/add.jsp
@@ -17,32 +17,38 @@
 
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">UsersCRUD</h1>
+        <h1 class="h3 mb-0 text-gray-800">UsersCRUD </h1>
         <a href="/user/list" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                 class="fas fa-download fa-sm text-white-50"></i> Lista Użytkowników</a>
     </div>
     <div class="card shadow mb-4">
         <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-primary">Dodawanie nowego użytkownika</h6>
+            <h1 class="h3 mb-0 text-danger">${message}</h1>
         </div>
         <div class="card-body">
+
             <form method="post">
                 <div class="form-group">
                     <label for="userName">Nazwa</label>
                     <input type="text" name="userName" class="form-control"
-                           placeholder="Nazwa użytkownika" id="userName">
+                           placeholder="Nazwa użytkownika" id="userName"
+                           value="${empty param.userName ? null : param.userName}">
                 </div>
                 <div class="form-group">
                     <label for="userName">Email</label>
                     <input type="text" name="userEmail" class="form-control"
-                           placeholder="Email użytkownika" id="userEmail">
+                           placeholder="Email użytkownika" id="userEmail" value="${empty userEmail ? null: userEmail}">
                 </div>
                 <div class="form-group">
                     <label for="userName">Hasło</label>
                     <input type="text" name="userPassword" class="form-control"
-                           placeholder="Hasło użytkownika" id="userPassword">
+                           placeholder="Hasło użytkownika" id="userPassword"
+                           value="${empty userPassword ? null : userPassword}">
                 </div>
                 <button type="submit" class="btn btn-success">Zapisz</button>
+
+
             </form>
         </div>
     </div>
